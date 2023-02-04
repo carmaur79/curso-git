b4880574018f9e35563b5f3396f76708677146a3
CIsaza <carlosmauricioisaza@hotmail.com>
2023-02-04 11:02:58 -0500

    Se guarda con los primeros temas vistos de Linkedin learning

---
+++ js/app.js
@@ -0,0 +1,17 @@
+"Use strict"
+//alert("Hola desde un archivo externo");
+
+var owner = "Mauricio Isaza";
+console.log(owner)
+
+function inicio (){
+    let owner= "Mauro";
+    console.log(owner)
+
+    let age = 43;
+    console.log(age)
+}
+inicio();
+
+var edad = 43;
+console.log(edad)
